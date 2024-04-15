b9fe2318671136fc114b82a23e20e848a786d2d9
aleg448 <90354487+aleg448@users.noreply.github.com>
2024-04-15 23:39:41 +0200

    modified to run on railway
    does what it says on the can

--- Dockerfile
+++ Dockerfile
@@ -1,7 +1,7 @@
-
 FROM ubuntu:22.04
 
 ENV DEBIAN_FRONTEND noninteractive
+
 RUN sed 's/main$/main universe/' -i /etc/apt/sources.list
 RUN apt-get upgrade -y
 RUN apt-get update
@@ -10,17 +10,18 @@ RUN apt-get update
 RUN apt-get install -y -f build-essential xorg libssl-dev libxrender-dev wget git curl
 RUN apt-get install -y --no-install-recommends xvfb libfontconfig libjpeg-turbo8 xfonts-75dpi fontconfig
 RUN apt-get install -y vim wkhtmltopdf libssl-dev unixodbc
-RUN apt-get install -y poppler-utils tesseract-ocr libtesseract-dev 
+RUN apt-get install -y poppler-utils tesseract-ocr libtesseract-dev
 RUN apt-get update
 
 ENV HOME /home/vectara
 ENV XDG_RUNTIME_DIR=/tmp
+
 WORKDIR ${HOME}
+
 SHELL ["/bin/bash", "-c"]
 
 RUN apt-get install -y python3-pip
 RUN pip3 install poetry
-
 COPY poetry.lock pyproject.toml $HOME/
 RUN poetry config virtualenvs.create false
 RUN poetry install --only main
@@ -30,13 +31,20 @@ RUN playwright install --with-deps firefox
 ARG INSTALL_EXTRA=false
 COPY requirements.txt $HOME/
 RUN if [ "$INSTALL_EXTRA" = "true" ]; then \
-        pip3 install -r requirements.txt && \
-        python3 -m spacy download en_core_web_lg; \
+    pip3 install -r requirements.txt && \
+    python3 -m spacy download en_core_web_lg; \
     fi
 
 COPY *.py $HOME/
 COPY core/*.py $HOME/core/
 COPY crawlers/ $HOME/crawlers/
 
+# Set environment variables
+ENV CONFIG_FILE=/home/vectara/env/config.yaml
+ENV PROFILE=$PROFILE
+ENV VECTARA_API_KEY=$VECTARA_API_KEY
+ENV VECTARA_CORPUS_ID=$VECTARA_CORPUS_ID
+ENV VECTARA_CUSTOMER_ID=$VECTARA_CUSTOMER_ID
+
 ENTRYPOINT ["/bin/bash", "-l", "-c"]
-CMD ["python3 ingest.py $CONFIG $PROFILE"]
+CMD ["python3", "ingest.py"]
--- run.sh
+++ run.sh
@@ -1,29 +1,25 @@
 #!/bin/bash
 
-# args[1] = config file
-# args[2] = secrets profile
-# example: sh run.sh <config>news-bbc.yaml dev
-
-if [ $# -lt 2 ]; then
-  echo "Missing arguments."
-  echo "Usage: $0 <config-file> <secrets-profile>"
-  exit 1
+if [ -z "$CONFIG_FILE" ]; then
+    echo "Error: CONFIG_FILE environment variable is not set"
+    exit 1
 fi
 
-if [ ! -f "$1" ]; then
-  echo "Error: '$1' is not a valid configuration file"
-  exit 2
+if [ -z "$PROFILE" ]; then
+    echo "Error: PROFILE environment variable is not set"
+    exit 2
 fi
 
-if [ ! -f secrets.toml ]; then
-  echo "Error: secrets.toml file does not exist, please create one following the README instructions"
-  exit 3
+if [ ! -f "$CONFIG_FILE" ]; then
+    echo "Error: '$CONFIG_FILE' is not a valid configuration file"
+    exit 3
 fi
 
-# Mount secrets file into docker container
-mkdir -p ~/tmp/mount
-cp secrets.toml ~/tmp/mount
-cp $1 ~/tmp/mount/
+# Mount config file into docker container
+mkdir -p ~/tmp/mount/config
+cp $CONFIG_FILE ~/tmp/mount/config/
+
+config_file_name="${CONFIG_FILE##*/}"
 
 # Build docker container
 ARCH=$(uname -m)
@@ -33,59 +29,62 @@ fi
 
 # Determine the build command based on the availability of Buildx
 function has_buildx() {
-  docker buildx version > /dev/null 2>&1
+    docker buildx version > /dev/null 2>&1
 }
+
 if has_buildx; then
-  BUILD_CMD="buildx build"
-  echo "Building for $ARCH with buildx"
+    BUILD_CMD="buildx build"
+    echo "Building for $ARCH with buildx"
 else
-  BUILD_CMD="build"
-  echo "Building for $ARCH"
+    BUILD_CMD="build"
+    echo "Building for $ARCH"
 fi
 
-sum_tables=`python3 -c "import yaml; print(yaml.safe_load(open('$1'))['vectara'].get('summarize_tables', 'false'))" | tr '[:upper:]' '[:lower:]'`
-mask_pii=`python3 -c "import yaml; print(yaml.safe_load(open('$1'))['vectara'].get('mask_pii', 'false'))" | tr '[:upper:]' '[:lower:]'`
+sum_tables=`python3 -c "import yaml; print(yaml.safe_load(open('$CONFIG_FILE'))['vectara'].get('summarize_tables', 'false'))" | tr '[:upper:]' '[:lower:]'`
+mask_pii=`python3 -c "import yaml; print(yaml.safe_load(open('$CONFIG_FILE'))['vectara'].get('mask_pii', 'false'))" | tr '[:upper:]' '[:lower:]'`
+
 tag="vectara-ingest"
+
 if [[ "$sum_tables" == "true" || "$mask_pii" == "true" ]]; then
     echo "Building with extra features"
     docker $BUILD_CMD --build-arg INSTALL_EXTRA="true" --platform linux/$ARCH . --tag="$tag:latest"
 else
-  docker $BUILD_CMD --build-arg INSTALL_EXTRA="false" --platform linux/$ARCH . --tag="$tag:latest"
+    docker $BUILD_CMD --build-arg INSTALL_EXTRA="false" --platform linux/$ARCH . --tag="$tag:latest"
 fi
 
 if [ $? -eq 0 ]; then
-  echo "Docker build successful."
+    echo "Docker build successful."
 else
-  echo "Docker build failed. Please check the messages above. Exiting..."
-  exit 4
+    echo "Docker build failed. Please check the messages above. Exiting..."
+    exit 4
 fi
 
-# remove old container if it exists
+# Remove old container if it exists
 docker container inspect vingest &>/dev/null && docker rm -f vingest
 
 # Run docker container
-crawler_type=`python3 -c "import yaml; print(yaml.safe_load(open('$1'))['crawling']['crawler_type'])" | tr '[:upper:]' '[:lower:]'`
-config_file_name="${1##*/}"
+crawler_type=`python3 -c "import yaml; print(yaml.safe_load(open('$CONFIG_FILE'))['crawling']['crawler_type'])" | tr '[:upper:]' '[:lower:]'`
+
 if [[ "${crawler_type}" == "folder" ]]; then
-    # special handling of "folder crawler" where we need to mount the folder under /home/vectara/data
-    folder=`python3 -c "import yaml; print(yaml.safe_load(open('$1'))['folder_crawler']['path'])"`
+    # Special handling of "folder crawler" where we need to mount the folder under /home/vectara/data
+    folder=`python3 -c "import yaml; print(yaml.safe_load(open('$CONFIG_FILE'))['folder_crawler']['path'])"`
     echo $folder
-    docker run -d -v ~/tmp/mount:/home/vectara/env -v "$folder:/home/vectara/data" -e CONFIG=/home/vectara/env/$config_file_name -e PROFILE=$2 --name vingest $tag
+    docker run -d -v "$folder:/home/vectara/data" -e CONFIG=/home/vectara/env/$config_file_name -e PROFILE=$PROFILE -e VECTARA_API_KEY=$VECTARA_API_KEY -e VECTARA_CORPUS_ID=$VECTARA_CORPUS_ID -e VECTARA_CUSTOMER_ID=$VECTARA_CUSTOMER_ID --name vingest $tag
 elif [[ "$crawler_type" == "csv" ]]; then
-    # special handling of "csv crawler" where we need to mount the csv file under /home/vectara/data
-    csv_path=`python3 -c "import yaml; print(yaml.safe_load(open('$1'))['csv_crawler']['csv_path'])"`
-    docker run -d -v ~/tmp/mount:/home/vectara/env -v "$csv_path:/home/vectara/data/file.csv" -e CONFIG=/home/vectara/env/$config_file_name -e PROFILE=$2 --name vingest $tag
-elif [[ "$crawler_type" == "buldupload" ]]; then
-    # special handling of "bulkupload crawler" where we need to mount the JSON file under /home/vectara/data
-    json_path=`python3 -c "import yaml; print(yaml.safe_load(open('$1'))['bulkupload_crawler']['json_path'])"`
-    docker run -d -v ~/tmp/mount:/home/vectara/env -v "$json_path:/home/vectara/data/file.json" -e CONFIG=/home/vectara/env/$config_file_name -e PROFILE=$2 --name vingest $tag
+    # Special handling of "csv crawler" where we need to mount the csv file under /home/vectara/data
+    csv_path=`python3 -c "import yaml; print(yaml.safe_load(open('$CONFIG_FILE'))['csv_crawler']['csv_path'])"`
+    docker run -d -v "$csv_path:/home/vectara/data/file.csv" -e CONFIG=/home/vectara/env/$config_file_name -e PROFILE=$PROFILE -e VECTARA_API_KEY=$VECTARA_API_KEY -e VECTARA_CORPUS_ID=$VECTARA_CORPUS_ID -e VECTARA_CUSTOMER_ID=$VECTARA_CUSTOMER_ID --name vingest $tag
+elif [[ "$crawler_type" == "bulkupload" ]]; then
+    # Special handling of "bulkupload crawler" where we need to mount the JSON file under /home/vectara/data
+    json_path=`python3 -c "import yaml; print(yaml.safe_load(open('$CONFIG_FILE'))['bulkupload_crawler']['json_path'])"`
+    docker run -d -v "$json_path:/home/vectara/data/file.json" -e CONFIG=/home/vectara/env/$config_file_name -e PROFILE=$PROFILE -e VECTARA_API_KEY=$VECTARA_API_KEY -e VECTARA_CORPUS_ID=$VECTARA_CORPUS_ID -e VECTARA_CUSTOMER_ID=$VECTARA_CUSTOMER_ID --name vingest $tag
 else
-    docker run -d -v ~/tmp/mount:/home/vectara/env -e CONFIG=/home/vectara/env/$config_file_name -e PROFILE=$2 --name vingest $tag
+    docker run -d -e CONFIG=/home/vectara/env/$config_file_name -e PROFILE=$PROFILE -e VECTARA_API_KEY=$VECTARA_API_KEY -e VECTARA_CORPUS_ID=$VECTARA_CORPUS_ID -e VECTARA_CUSTOMER_ID=$VECTARA_CUSTOMER_ID --name vingest $tag
 fi
 
 if [ $? -eq 0 ]; then
-  echo "Success! Ingest job is running."
-  echo "You can try 'docker logs -f vingest' to see the progress."
+    echo "Success! Ingest job is running."
+    echo "You can try 'docker logs -f vingest' to see the progress."
 else
-  echo "Ingest container failed to start. Please check the messages above."
+    echo "Ingest container failed to start. Please check the messages above."
 fi
